2bfe86b0aa220d1b2fe0de8bd3d8c427ce223035
EtienneMangino <etiennemangino@gmail.com>
2014-11-12 11:36:49 +0100

    models and controllers generated

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,13 @@
+class RestaurantsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'reviews/show'
+
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
+  get 'restaurants/index'
+
+  get 'restaurants/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
